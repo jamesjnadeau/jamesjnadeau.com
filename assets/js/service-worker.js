55ba01dce5cced3875674b002eb10bf13996dc12
jamesjnadeau <james.nadeau@gmail.com>
2018-12-14 01:45:36 +0000

    :tada: atempting to use sw-tools for service workers

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,4 +1,7 @@
+var toolbox = require('sw-toolbox');
+
 var version = env.version;
+
 
 // resources required for offline access.
 var offlineResources = [
@@ -12,65 +15,38 @@ var offlineResources = [
   "/frontend.js?v=" + version,
 ];
 
-self.addEventListener("install", function(event) {
-  event.waitUntil(
-    caches
-      .open("static")
-      .then(function(cache) {
-        cache.addAll(offlineResources);
-      })
-  );
+// Turn on debug logging, visible in the Developer Tools' console.
+toolbox.options.debug = true;
+
+// cache during the service worker install step
+toolbox.precache(offlineResources);
+
+// offline block - from https://github.com/GoogleChromeLabs/sw-toolbox/issues/80#issuecomment-171772793
+toolbox.router.get('/(.*)', function(request, values, options) {
+  // networkFirst will attempt to return a response from the network,
+  // then attempt to return a response from the cache.
+  return toolbox.networkFirst(request, values, options).catch(function(error) {
+    // If both the network and the cache fail, then `.catch()` will be triggered,
+    // and we get a chance to respond with our cached fallback page.
+    // This would ideally check event.request.mode === 'navigate', but that isn't supported in
+    // Chrome as of M48. See https://fetch.spec.whatwg.org/#concept-request-mode
+    if (request.method === 'GET' && request.headers.get('accept').includes('text/html')) {
+      return toolbox.cacheOnly(new Request('/offline'), values, options);
+    }
+    throw error;
+  });
 });
 
-self.addEventListener("activate", function(event) {
-  event.waitUntil(
-    caches.keys().then(function(keys) {
-      console.log('killing all cache upon activation');
-      return Promise.all(keys
-        .filter(function (key) {
-          // if there's not 'static' in the key, it's something we should delete
-          return key.indexOf('static') === -1;
-        })
-        .map(function (key) {
-          return caches.delete(key);
-        })
-      );
-    })
-  );
+// By default, all requests that don't match our custom handler will use the
+// toolbox.networkFirst cache strategy, and their responses will be stored in
+// the default cache.
+toolbox.router.default = toolbox.networkFirst;
+
+// Boilerplate to ensure our service worker takes control of the page as soon
+// as possible.
+self.addEventListener('install', function(event) {
+  return event.waitUntil(self.skipWaiting());
 });
-
-function isOfflineOrigin(origin) {
-  return origin === location.origin;
-}
-
-self.addEventListener("fetch", function(event) {
-  console.log('fetching');
-  var request = event.request;
-  var url = new URL(request.url);
-
-  // throw out anything but get requests and domains we allow.
-  if (request.method !== "GET" || !isOfflineOrigin(url.origin)) {
-    return;
-  }
-
-  // look in the cache first, and fall back to the network
-  event.respondWith(
-    caches.match(request)
-      .then(function(response) {
-        console.log('responding with cache', !!response);
-        return response || fetch(request).then(function(fetchResponse) {
-          // we have a successful request at this point, cache it?
-          var copy = fetchResponse.clone();
-          console.log('fetchResponse', copy.clone());
-          caches.open("swCache")
-            .then(function(cache) {
-              console.log('caching', request.url);
-              cache.put(request, copy);
-            });
-          return fetchResponse;
-        });
-      }).catch(function() {
-        return caches.match("/offline");
-      })
-  );
+self.addEventListener('activate', function(event) {
+  return event.waitUntil(self.clients.claim());
 });
