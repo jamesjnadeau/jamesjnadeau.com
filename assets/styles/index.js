7ad571ee275e69a86ecf43c6da53a7fa501204bf
jamesjnadeau <james.nadeau@gmail.com>
2019-04-11 10:30:29 +0000

    :tada: move to variable/less based bootstrap setup

--- assets/styles/index.js
+++ assets/styles/index.js
@@ -1,4 +1,5 @@
-require('bootstrap/dist/css/bootstrap.css');
+//require('bootstrap/dist/css/bootstrap.css');
+require('./Bootstrap.less');
 require('./default.css');
 require('./animate.css');
 require('./csspinner.min.css');
