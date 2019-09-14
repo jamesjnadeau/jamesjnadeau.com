37d508fbe1a867784b2ffee5b907ceb4fb1400a3
jamesjnadeau <james.nadeau@gmail.com>
2019-09-13 20:08:52 -0400

    :tada: add impress landing page and basic styles

--- assets/impress/index.js
+++ assets/impress/index.js
@@ -1,3 +1,5 @@
 require('impress.js/js/impress.js');
 
 window.impress().init();
+$('#impress-toolbar button').addClass('btn btn-outline-dark mx-1');
+$('#impress-toolbar select').addClass('form-control mx-1');
