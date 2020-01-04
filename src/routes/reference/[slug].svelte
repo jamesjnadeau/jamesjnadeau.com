8bd0ba45dc48945e14d4f3b74cec1ffa52847d7c
jamesjnadeau <james.nadeau@gmail.com>
2020-01-04 14:10:32 -0500

    :tada: fix github actions for sapper and add reference page

---
+++ src/routes/reference/[slug].svelte
@@ -0,0 +1,32 @@
+<script context="module">
+	let directory = 'reference';
+	export async function preload({ params, query }) {
+		// the `slug` parameter is available because
+		// this file is called [slug].svelte
+		
+		const res = await this.fetch(directory + `/${params.slug}.json`);
+		const data = await res.json();
+
+		if (res.status === 200) {
+			return { post: data };
+		} else {
+			this.error(res.status, data.message);
+		}
+	}
+</script>
+
+<script>
+	import { fadeIn, fadeOut } from "../_page_fade";
+	export let post;
+</script>
+
+
+<svelte:head>
+	<title>{post.title}</title>
+</svelte:head>
+
+<div in:fadeIn out:fadeOut>
+	<div class='content'>
+		{@html post.html}
+	</div>
+</div>
