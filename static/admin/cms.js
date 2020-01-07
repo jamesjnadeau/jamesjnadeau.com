5f3ec86d3b27aed07ca57f9be1b87196e043001c
jamesjnadeau <james.nadeau@gmail.com>
2020-01-06 20:35:35 -0500

    :tada: add cms styles for our site and temlate for TIL

--- static/admin/cms.js
+++ static/admin/cms.js
@@ -1,6 +1,7 @@
 import CMS from 'netlify-cms';
 import controlComponent from 'netlify-cms-widget-code/dist/esm/CodeControl';
 
+CMS.registerPreviewStyle('/styles/built/styles.css');
 
 var PugPreview = createClass({
   render: function() {
@@ -28,3 +29,18 @@ CMS.getWidget('pug').codeMirrorConfig = {
   mode: 'pug',
   theme: 'default',
 };
+
+var TILPreview = createClass({
+  render: function() {
+    var entry = this.props.entry;
+    return h('div', {},
+      h('navbar', {className: 'navbar navbar-expand-lg navbar-dark fixed-top', id:'header'}, 'James J Nadeau'),
+      // h('h1', {}, entry.getIn(['data', 'title'])),
+      h('div', { className: 'container', id: 'container'},
+        h('div', { className: "content"}, this.props.widgetFor('body'))
+      )
+    );
+  }
+});
+
+CMS.registerPreviewTemplate("TIL", TILPreview);
