861537b411517ac2340f12dd2bb9577c34eebe60
jamesjnadeau <james.nadeau@gmail.com>
2020-01-12 11:06:17 -0500

    :art: imporve variables defining page tranisitions so it's more flexible in the future

--- src/components/PageTransition.svelte
+++ src/components/PageTransition.svelte
@@ -1,29 +1,30 @@
 <script>
-    // from https://stackoverflow.com/a/57539275
+  // from https://stackoverflow.com/a/57539275
 	import { fly } from 'svelte/transition';
 
-    import { sineOut } from "svelte/easing";
+  import { sineOut } from "svelte/easing";
 
-    let duration = 500;
-    let delay = duration + 50;
+  let duration = 500;
+  let distance = 1000
 
-    let delayZero = 0;
+  let inDelay = duration + 50;
+  let outDelay = 0;
 
 
-    export const pageIn = {
-    x: 600,
+  export const pageIn = {
+    x: distance,
     duration,
-    delay,
-    }
+    inDelay,
+  }
 
 
-    export const pageOut = {
-    x: -600,
+  export const pageOut = {
+    x: -distance,
     duration,
-    delay: delayZero,
-    }
+    delay: outDelay,
+  }
 </script>
 
 <div in:fly={pageIn} out:fly={pageOut} >
-    <slot></slot>
+  <slot></slot>
 </div>
