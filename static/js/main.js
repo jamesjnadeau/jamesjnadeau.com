18d200bbffe1dfc07bf35314ad55752a2d393539
James <james.nadeau@gmail.com>
2025-03-15 03:16:09 +0000

    add netlify login widget

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 import 'bootstrap';
 import barba from '@barba/core';
 import Headroom from "headroom.js";
+import Combokeys from "combokeys";
 
 
 //
@@ -52,8 +53,45 @@ barba.init({
     }]
 });
 
-if(true) {
+/*
+    Content Editor section
+*/
+const start_editor = function() {
     import('../contentTools/editor.js').then(() => {
         console.log('Content tools loaded');
     });
-} 
+};
+const start_login = function() {
+    import('netlify-identity-widget').then((netlifyIdentity) => {
+        netlifyIdentity.init();
+        
+        // netlifyIdentity.open(); // open the modal
+        netlifyIdentity.open('login'); // open the modal to the login tab
+        // netlifyIdentity.open('signup'); // open the modal to the signup tab
+        
+        netlifyIdentity.on('init', user => console.log('init', user));
+        netlifyIdentity.on('login', user => {
+            console.log('login', user);
+            start_editor();
+        });
+        netlifyIdentity.on('logout', () => console.log('Logged out'));
+        netlifyIdentity.on('error', err => console.error('Error', err));
+        netlifyIdentity.on('open', () => console.log('Widget opened'));
+        netlifyIdentity.on('close', () => console.log('Widget closed'));
+    });
+};
+
+
+var combokeys = new Combokeys(document.documentElement);
+combokeys.bind(['command+l', 'ctrl+l'], function() {
+    console.log('command l or control l');
+    // return false to prevent default browser behavior
+    // and stop event from bubbling
+    start_login();
+    return false;
+});
+
+// konami code!
+combokeys.bind('up up down down left right left right b a enter', function() {
+    console.log('konami code');
+});
